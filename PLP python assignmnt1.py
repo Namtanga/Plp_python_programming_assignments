9a93698b5a8a1883e5898e2e5f5381bb76493127
Your Name <namtanga@gmail.com>
2024-03-03 14:23:29 +0300

    second commit

---
+++ PLP python assignmnt1.py
@@ -0,0 +1,15 @@
+#Use the input() function to ask the user for their name and store it in a variable called "name".
+name=input("what is you name")
+print('Enter your name')
+print('My name is', name)
+#Use the input() function to ask the user for their age and store it in a variable called "age".
+age=input('What is your age:')
+print('Enter your age')
+print('I am',age, 'years old')
+#Use the input() function to ask the user for their location and store it in a variable called "location".
+Location=input('where is your location:')
+print('Enter your location')
+print("I live at", Location)
+
+
+
